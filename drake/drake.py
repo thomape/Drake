f58dbd19b84aff2c183d0623e30d7ea2e42b986c
Thomas Errington <terrington@hirzel.com>
2020-10-28 09:25:42 -0400

    File structure & preface fx

---
+++ drake/drake.py
@@ -0,0 +1,30 @@
+
+
+def preface():
+    print('''
+    The Drake equation is designed to predict the liklihood of intelligent life elsewhere in the universe.
+    The following parameters are required to produce accurate results.
+
+        N= The number of civilizations in the Milky Way galaxy whose electromagnetic emissions are detectable.
+
+        R*= The rate of formation of stars suitable for the development of intelligent life (number per year).
+
+        fp= The fraction of those stars with planetary systems.
+
+        ne= The number of planets, per solar system, with an environment suitable for life.
+
+        fl= The fraction of suitable planets on which life actually appears.
+
+        fi= The fraction of life bearing planets on which intelligent life emerges.
+
+        fc= The fraction of civilizations that develop a technology that produces detectable signs of their existence.
+
+        L= The average length of time such civilizations produce such signs (years).
+    ''')
+    
+
+
+
+
+if __name__ == "__main__":
+    preface()
